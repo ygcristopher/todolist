4bf08023f720266a879e5a70605f894425609406
ygcristopher <ygca.dev@gmail.com>
2024-11-24 08:43:55 -0300

    Add task management routes: implement create and get tasks with user authentication

--- api/src/routes/routes.ts
+++ api/src/routes/routes.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import UserController from "../controllers/UserController";
+import TasksController from "../controllers/TasksController";
+import  UserAuth  from "../middleware/userAuth";
 
 const router = express.Router();
 
+// User routes
 router.post("/create-user", UserController.createUser);
 router.post("/login-user", UserController.loginUser);
 
+// Task routes
+router.post("/create-task/:userId", UserAuth, TasksController.createTask);
+router.get("/tasks/:userId", UserAuth, TasksController.getTasks);
 export default router;
