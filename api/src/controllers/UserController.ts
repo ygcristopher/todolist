404247ff782366493b6fe9314ee19d8b122ebe91
ygcristopher <ygca.dev@gmail.com>
2024-11-24 08:43:44 -0300

    Enhance UserController: add JWT authentication for login and improve error handling

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -1,30 +1,59 @@
 import { Request, Response } from "express";
 import { v4 as uuidv4 } from "uuid";
 import UserService from "../services/UserService";
+import jwt from "jsonwebtoken";
+import { config } from "dotenv";
+
+config()
+
+const access_token = process.env.ACCESS_TOKEN_SECRET;
 
 class UserController {
-    async createUser(req: Request, res: Response) {
-      const { name, email, password } = req.body;
+  async createUser(req: Request, res: Response) {
+    const { name, email, password } = req.body;
 
-        try{
-            const user = await UserService.createUser(uuidv4(), name, email, password);
-            return res.status(201).json(user)
-        }catch(error) {
-            console.log(error)
-        }
+    try {
+      const user = await UserService.createUser(
+        uuidv4(),
+        name,
+        email,
+        password
+      );
+
+      return res.status(201).json(user);
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    async loginUser(req: Request, res: Response) {
-        const { email, password } = req.body;
-
-        try {
-            const user = await UserService.loginUser(email, password);
-            return res.status(200).json(user);
-        } catch (error) {
-            console.log(error);
+  async loginUser(req: Request, res: Response) {
+    const { email, password } = req.body;
+  
+    try {
+      const userEmail = await UserService.getUserByEmail(email);
+  
+      if (!userEmail) {
+        return res.status(404).json({ message: "User not found" });
+      }
+  
+      const user = await UserService.loginUser(email, password);
+  
+      if (!user) {
+        return res.status(401).json({ message: "Invalid credentials" });
+      }
+  
+      const token = jwt.sign(
+        { id: userEmail.id, email: userEmail.email },
+        access_token || "", 
+        { expiresIn: "1h" } 
+      );
+  
+      return res.status(200).json({ user, token });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Internal server error" });
     }
- }
-}
+  }
+}  
 
 export default new UserController();
-
