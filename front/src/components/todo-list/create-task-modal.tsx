ee471b0b6ce859fe6e66332da642c9fbfd84c4fc
ygcristopher <ygca.dev@gmail.com>
2024-11-26 12:56:58 -0300

    Enhance create task modal: add priority selection and toast notifications for task creation feedback

--- front/src/components/todo-list/create-task-modal.tsx
+++ front/src/components/todo-list/create-task-modal.tsx
@@ -1,11 +1,17 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from "react";
-import { Dialog } from "../ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogOverlay,
+  DialogTitle,
+} from "@radix-ui/react-dialog";
 import { Input } from "../ui/input";
 import { Button } from "@/components/ui/button";
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
+import { useToast } from "@/hooks/use-toast";
 
 interface CreateTaskModalProps {
   isOpen: boolean;
@@ -20,7 +26,10 @@ const CreateTaskModal = ({
 }: CreateTaskModalProps) => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [priority, setPriority] = useState("");
   const [userId, setUserId] = useState<string | null>(null);
+
+  const { toast } = useToast();
 
   useEffect(() => {
     const getToken = localStorage.getItem("token");
@@ -32,9 +41,9 @@ const CreateTaskModal = ({
 
   const createTask = async () => {
     try {
-      await axios.post(
+      const response = await axios.post(
         `http://localhost:3003/create-task/${userId}`,
-        { title, description },
+        { title, description, priority },
         {
           headers: {
             "Content-Type": "application/json",
@@ -43,29 +52,73 @@ const CreateTaskModal = ({
         }
       );
 
+      const data = response.data;
+
+      if (data.error) {
+        toast({
+          title: "Error",
+          description: data.error || "An error occurred",
+          variant: "error",
+        });
+        return;
+      }
+
+      toast({
+        title: "Success",
+        description: data.message,
+        variant: "success",
+      });
+
       onTaskCreated();
       onClose();
     } catch (error) {
-      console.error("Erro ao criar tarefa:", error);
+      console.error("Error created task", error);
+      toast({
+        title: "Error",
+        description: "Internal server error. Please try again later.",
+        variant: "error",
+      });
     }
   };
 
   return (
-    <Dialog open={isOpen}>
-      <div className="p-4 space-y-4">
-        <h2 className="text-lg font-bold">Adicionar Tarefa</h2>
-        <Input
-          placeholder="Título"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <Input
-          placeholder="Descrição"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        <Button onClick={createTask}>Salvar</Button>
-      </div>
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogOverlay className="fixed inset-0 bg-black/50 z-50" />
+      <DialogContent className="fixed bg-white p-4 rounded shadow-lg max-w-md w-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
+        <DialogTitle className="text-lg font-bold mb-4">
+          Adicionar Tarefa
+        </DialogTitle>
+        <div className="space-y-4">
+          <Input
+            placeholder="Título"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <Input
+            placeholder="Descrição"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <select
+            value={priority}
+            onChange={(e) => setPriority(e.target.value)}
+            className="p-2 rounded-md bg-gray-100 w-full"
+          >
+            <option value="">Selecione a prioridade</option>
+            <option value="BAIXA">BAIXA</option>
+            <option value="MÉDIA">MÉDIA</option>
+            <option value="ALTA">ALTA</option>
+          </select>
+          <div className="flex justify-end space-x-2">
+            <Button variant="outline" onClick={onClose}>
+              Cancelar
+            </Button>
+            <Button onClick={createTask} disabled={!title || !description}>
+              Salvar
+            </Button>
+          </div>
+        </div>
+      </DialogContent>
     </Dialog>
   );
 };
