f73d45c0f68294d6a97413c731448210ab7410ce
ygcristopher <ygca.dev@gmail.com>
2024-11-27 10:04:22 -0300

    Refactor UserService: improve code formatting and enhance readability

--- api/src/services/UserService.ts
+++ api/src/services/UserService.ts
@@ -2,7 +2,13 @@ import knex from "../database/connection";
 import bcrypt from "bcryptjs";
 
 class UserService {
-  async createUser(id: string, name: string, email: string, password: string, bg_profile: string) {
+  async createUser(
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+    bg_profile: string
+  ) {
     try {
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(password, salt);
@@ -22,25 +28,28 @@ class UserService {
   async loginUser(email: string, password: string) {
     try {
       const user = await knex("users").where("email", email).first();
-  
+
       if (!user) {
         throw new Error("User not found");
       }
-  
+
       const isPasswordValid = await bcrypt.compare(password, user.password);
       if (!isPasswordValid) {
         throw new Error("Invalid password");
       }
-  
+
       return user;
     } catch (error) {
       throw error;
     }
   }
-  
+
   async getUserByEmail(email: string) {
     try {
-      const user = await knex("users").select('*').where("email", email).first();
+      const user = await knex("users")
+        .select("*")
+        .where("email", email)
+        .first();
       console.log(user);
 
       return user;
